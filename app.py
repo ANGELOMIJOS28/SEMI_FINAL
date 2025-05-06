e3592ca256a728ea6bcfc4e18665d04885805304
ANGELOMIJOS28 <mijosangelo123@gmail.com>
2025-05-06 17:55:25 +0800

    Initial Commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return ("Angelo S. Mijos "
+            "BSIT 3A"
+            "SYSTEM INTEGRATION"
+            "SEMI FINAL EXAM")
+
+if __name__ == "__main__":
+    # app.run(debug=True)
+    port = int(os.inviron.get("PORT", 5000))
+    app.run(host='0.0.0.0',port=port)
