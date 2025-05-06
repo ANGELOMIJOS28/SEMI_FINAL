aefc2845cdeadebf3e1e2d4ba37dbaada74934dc
ANGELOMIJOS28 <mijosangelo123@gmail.com>
2025-05-06 18:32:30 +0800

    update

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ def home():
             "SEMI FINAL EXAM")
 
 if __name__ == "__main__":
-    # app.run(debug=True)
+    app.run(debug=True)
     port = int(os.inviron.get("PORT", 5000))
     app.run(host='0.0.0.0',port=port)
